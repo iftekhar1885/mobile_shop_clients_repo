e5356465361aaa3287bb3c559af0141d2476ae0c
mdefat233 <kharifte16@gmail.com>
2022-11-28 01:02:45 +0600

    added service card

--- src/pages/Dashboard/MyProducts/MyProductsDetails.js
+++ src/pages/Dashboard/MyProducts/MyProductsDetails.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MyProductsDetails = ({product}) => {
+    const [deleteProduct, setDeleteProduct] = useState([])
+
     const {_id, product_name, image, price, previous, mobile, description, condition, location} = product;
+
+    const handleDelete = id =>{
+        const proceed = window.confirm('Are you sure, you want to delete this products')
+        if(proceed){
+            fetch(`http://localhost:5000/sell/${id}`, {
+                method: 'DELETE'
+
+            })
+            .then(res => res.json())
+            .then(data =>{
+                console.log(data);
+                if(data.deletedCount > 0){
+                    alert('deleted successfully');
+                    const remaining = deleteProduct.filter(details => details._id !== id);
+                    setDeleteProduct(remaining);
+                }
+            })
+        }
+    }
 
     return (
         <div className="card w-96 bg-base-100 shadow-xl mt-20">
@@ -19,7 +40,7 @@ const MyProductsDetails = ({product}) => {
                 <p className='text-blue-400'>{description}</p>
                 <div className="card-actions justify-end">
                     <div className="badge badge-outline">Edit</div>
-                    {/* <div onClick={() => handleDelete(_id)} className="badge badge-outline">Delete</div> */}
+                    <div onClick={() => handleDelete(_id)} className="badge badge-outline">Delete</div>
                 </div>
             </div>
         </div>
